10991b928a03872aa68be1f5415cd0fe929881da
Miriam <miriam.haenle@gmail.com>
2021-02-18 18:52:43 +0100

    Second question

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import Card from './Card';
 import Button from './Button';
 
 function App() {
+  // const [state, functionToUpdateState] = useState(initialState)
   const [isShowingDetails, setIsShowingDetails] = useState(false);
+
+  const fruits = ['apple', 'banana', 'strawberry', 'papaya'];
+
+  const apple = fruits[0];
+  const banana = fruits[1];
+  const strawberry = fruits[2];
+
+  //destructuring of arrays
+
+  const [one, two, three] = fruits;
 
   function toggleDetails() {
     setIsShowingDetails(!isShowingDetails);
@@ -16,11 +27,7 @@ function App() {
   return (
     <>
       <h1>Hello to OMR React Basics!</h1>
-      <Card currywurst={isShowingDetails} />
-      <Button
-        text={isShowingDetails ? 'Hide' : 'Show'}
-        clickHandler={toggleDetails}
-      />
+      <Card currywurst={isShowingDetails} onToggleDetails={toggleDetails} />
       <Button
         text="Logg me something"
         clickHandler={() => logSomeStuff('OMR React Basics')} //Wenn eine Funktion einen Parameter übegeben bekommt, muss ich sie als Arrow Funktion an meine Komponente übergeben
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,10 @@
-export default function Card({ currywurst }) {
+import Button from './Button';
+
+export default function Card({ currywurst, onToggleDetails }) {
   return (
     <div>
       <h2>React is awesome</h2>
+
       {currywurst && (
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ducimus
@@ -9,6 +12,11 @@ export default function Card({ currywurst }) {
           consequuntur.
         </p>
       )}
+
+      <Button
+        text={currywurst ? 'Hide' : 'Show'}
+        clickHandler={onToggleDetails}
+      />
     </div>
   );
 }
